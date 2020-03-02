2ef626a17bd90a88ff6ad683582f2473879cd57c
la.abramkin <la.abramkin@kernel.ua>
2020-03-02 18:37:49 +0200

    first commit

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@workshop/material';
+import { HomeModule } from './home/home.module';
+import { ProjectsModule } from './projects/projects.module';
+import { CustomersModule } from './customers/customers.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    MaterialModule,
+    HomeModule,
+    ProjectsModule,
+    CustomersModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
